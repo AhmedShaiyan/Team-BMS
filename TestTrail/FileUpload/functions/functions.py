6635f51e8b2bb9d1cc3d6902640b113037c83bb4
somebodyelsedidthis <soham.pujari@gmail.com>
2023-07-23 02:43:00 +0800

    amended job rec algorithm

--- TestTrail/FileUpload/functions/functions.py
+++ TestTrail/FileUpload/functions/functions.py
@@ -3,7 +3,10 @@ from pdfminer.high_level import extract_text
 import json
 import psycopg2
 import numpy as np
+import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.sparse import csr_matrix, coo_matrix
 from FileUpload.models import Document
 import environ
 
@@ -20,7 +23,7 @@ def identify_skills():
 
     completion = openai.ChatCompletion.create(
         model = 'gpt-3.5-turbo',
-        messages = [{'role':'system','content':"Return a python list of the top 10 skills identified in the following resume, summarising them in up to 3 words each: " + text}],
+        messages = [{'role':'system','content':"Return a Python list of the top 10 skills identified in the following resume, summarising them in up to 3 words each: " + text}],
         max_tokens = 1000,
         n = 3,
         stop = None,
@@ -40,26 +43,51 @@ def connect_to_database():
         port=env('DBPORT')
     )
 
-
     return connection
 
-def retrieve_job_listings(conn):
-    cursor = conn.cursor()
-    cursor.execute("SELECT job, skills FROM joblistings")
-    job_listings = cursor.fetchall()
-    cursor.close()
-    return job_listings
+def fetchjoblistings():
+    conn = connect_to_database()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM joblistings;')
+    temp_list = cur.fetchall()
+    cur.close()
 
-def calculate_cosine_similarity(skills, job_listings):
-    skill_set = set(skills)
-    similarities = []
+    df = pd.DataFrame(temp_list,columns=('job','skills'))
     
-    for job_title, job_skills in job_listings:
-        job_skill_set = set(job_skills)
-        common_skills = skill_set.intersection(job_skill_set)
-        
-        # Calculate the cosine similarity
-        similarity = len(common_skills) / (np.sqrt(len(skill_set)) * np.sqrt(len(job_skill_set)))
-        similarities.append((job_title, similarity))
+    return df
+
+def calculate_cosine_similarity():
     
-    return similarities
+    df = fetchjoblistings()
+    
+    #change to append skill output to end
+    df.loc[len(df)] = ['Test',"['Detail-oriented', 'Six Sigma Black Belt', 'Manufacturing internships', 'Efficiency improvement', 'Cost reduction', 'Setup reduction', 'Kanban implementation', 'Mechanical engineering', 'MATLAB, AutoCAD, SolidWorks', 'CNC, Lathes']"]
+    
+    tv = TfidfVectorizer()
+    tfidf_matrix = tv.fit_transform(df['skills'])
+
+    A_sparse = csr_matrix(tfidf_matrix)
+    similarities_sparse = cosine_similarity(A_sparse, dense_output = True)
+    table_len = len(similarities_sparse)
+    similarity_list = []
+
+    for i in range(len(similarities_sparse[table_len-1])):
+        similarity_list.append(similarities_sparse[table_len-1][i])
+    
+    del similarity_list[-1]
+    sim_list_copy = similarity_list.copy()
+
+    index_list = []
+
+    for j in range(5):
+        i = sim_list_copy.index(max(sim_list_copy))
+        index_list.append(i)
+        del sim_list_copy[i]
+
+    job_rec = []
+
+    for item in index_list:
+        job_rec.append(df['job'][item])
+
+    return job_rec
+
